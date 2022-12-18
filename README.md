0c6bf9e61eab8a4d4e7363ddca47efef887b6652
Alexandre Smirnov <sashasmart@gmail.com>
2022-12-17 22:40:42 -0800

    handle multiple words

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
+### Pitch Diagrammer
+
+This repo contains a python script to generate pitch accent diagrams for languages like Japanese or Korean.  It comes with several command-line options for setting things like size parameters, the desired word, and the desired pitch pattern.
+
+Examples: 
+- `python3 draw.py --pitch_pattern LHLLH --text_string 감사합니다 --height 150 --step 100 --padding 10 --outer_point_radius 10 --inner_point_radius 8 --output_file example.svg`
+- `python3 draw.py --pitch_pattern LHLLH --text_string 감사합니다`
+
 ### Resources
 - https://seriot.ch/pycairo/#3_clip_and_mask.clip
 - https://www.cairographics.org/cookbook/outside_clipping/
--- draw.py
+++ draw.py
@@ -4,9 +4,21 @@ import xml.etree.ElementTree as ET
 import argparse
 
 
+class PitchWord:
+    def __init__(
+        self, pitch_pattern: str, text_string: str, points: list[tuple[float, float]]
+    ):
+        self.pitch_pattern = pitch_pattern
+        self.text_string = text_string
+        self.points = points
+
+    def __repr__(self):
+        return f"{{{self.text_string}: {self.pitch_pattern}: {self.points}}}"
+
+
 def generate_svg(
-    pitch_pattern: str,
-    text_string: str,
+    pitch_pattern_list: list[str],
+    text_string_list: list[str],
     height: float,
     step: float,
     padding: float,
@@ -15,19 +27,38 @@ def generate_svg(
     output_file: str,
 ) -> None:
 
+    if len(pitch_pattern_list) != len(text_string_list):
+        raise Exception("# words mismatch between pitch patterns and text strings")
+
     edge_offset = outer_point_radius + padding
 
-    width = ((len(pitch_pattern) - 1) * step) + (outer_point_radius * 2) + (padding * 2)
+    pitch_words: list[PitchWord] = []
+    total_point_count = 0
 
-    points: tuple[float, float] = []
+    for i, pitch_pattern in enumerate(pitch_pattern_list):
+        text_string = text_string_list[i]
+        if len(pitch_pattern) != len(text_string):
+            raise Exception("length mismatch between a pitch pattern and text string")
 
-    for i, char in enumerate(pitch_pattern):
-        curr_x = edge_offset + (step * i)
-        curr_y = edge_offset if char == "H" else height - edge_offset
+        points: list[tuple[float, float]] = []
+        starting_x = total_point_count * step
 
-        points.append((curr_x, curr_y))
+        for i, char in enumerate(pitch_pattern):
+            curr_x = starting_x + edge_offset + (step * i)
+            curr_y = edge_offset if char == "H" else height - edge_offset
 
-    print(points)
+            points.append((curr_x, curr_y))
+            total_point_count = total_point_count + 1
+
+        pitch_words.append(
+            PitchWord(
+                pitch_pattern,
+                text_string,
+                points,
+            )
+        )
+
+    width = (padding * 2) + (total_point_count * step) + (outer_point_radius * 2)
 
     with cairo.SVGSurface(output_file, width, height) as surface:
         context = cairo.Context(surface)
@@ -38,59 +69,65 @@ def generate_svg(
 
         context.set_source_rgba(0, 0, 0, 1)
 
-        # punch out each point into the clip area
-        for x, y in points:
-            context.arc(x, y, outer_point_radius, 0, 2 * math.pi)
+        total_point_count = 0
+
+        for word in pitch_words:
+
+            # punch out each point into the clip area
+            for x, y in word.points:
+                context.arc(x, y, outer_point_radius, 0, 2 * math.pi)
+                context.new_sub_path()
+
+            # draw big rectangle around whole area
+            context.move_to(0, 0)
+            context.line_to(0, height)
+            context.line_to(width, height)
+            context.line_to(width, 0)
+            context.close_path()
+
+            # clip area between punched-out circles and outer edges
+            context.clip()
+
+            # start drawing actual diagram
+            context.set_source_rgba(0, 0, 0, 1)
+            context.set_line_width(10)
+
+            # draw lines
             context.new_sub_path()
+            for x, y in word.points:
+                context.line_to(x, y)
 
-        # draw big rectangle around whole area
-        context.move_to(0, 0)
-        context.line_to(0, height)
-        context.line_to(width, height)
-        context.line_to(width, 0)
-        context.close_path()
-
-        # clip area between punched-out circles and outer edges
-        context.clip()
-
-        # start drawing actual diagram
-        context.set_source_rgba(0, 0, 0, 1)
-        context.set_line_width(10)
-
-        # draw lines
-        context.new_sub_path()
-        for x, y in points:
-            context.line_to(x, y)
-
-        context.stroke()
-
-        # now draw each actual point
-        for x, y in points:
-            context.arc(x, y, inner_point_radius + 2, 0, 2 * math.pi)
             context.stroke()
+
+            # now draw each actual point
+            for x, y in word.points:
+                context.arc(x, y, inner_point_radius + 2, 0, 2 * math.pi)
+                context.stroke()
 
     # now add text
     ET.register_namespace("", "http://www.w3.org/2000/svg")
     tree = ET.parse(output_file)
     root = tree.getroot()
 
-    for i, (x, y) in enumerate(points):
-        letter = text_string[i]
+    for word in pitch_words:
 
-        font_size = inner_point_radius * 1.3
+        for i, (x, y) in enumerate(word.points):
+            letter = word.text_string[i]
 
-        text_element = ET.Element(
-            "text",
-            attrib={
-                "x": f"{x}",
-                "y": f"{y}",
-                "style": f"font-size: {font_size}px; dominant-baseline: middle; text-anchor:middle",
-            },
-        )
-        text_element.text = letter
-        root.append(text_element)
+            font_size = inner_point_radius * 1.3
 
-    tree.write(output_file)
+            text_element = ET.Element(
+                "text",
+                attrib={
+                    "x": f"{x}",
+                    "y": f"{y}",
+                    "style": f"font-size: {font_size}px; dominant-baseline: middle; text-anchor:middle",
+                },
+            )
+            text_element.text = letter
+            root.append(text_element)
+
+        tree.write(output_file)
 
 
 def main():
@@ -99,8 +136,16 @@ def main():
         description="Generate an SVG of a pitch diagram with specified parameters",
     )
 
-    parser.add_argument("--pitch_pattern", type=str)
-    parser.add_argument("--text_string", type=str)
+    parser.add_argument(
+        "--pitch_pattern_list",
+        type=str,
+        nargs="+",
+    )
+    parser.add_argument(
+        "--text_string_list",
+        type=str,
+        nargs="+",
+    )
     parser.add_argument("--height", type=float, default=150)
     parser.add_argument("--step", type=float, default=100)
     parser.add_argument("--padding", type=float, default=10)
@@ -110,8 +155,8 @@ def main():
     args = parser.parse_args()
 
     generate_svg(
-        pitch_pattern=args.pitch_pattern,
-        text_string=args.text_string,
+        pitch_pattern_list=args.pitch_pattern_list,
+        text_string_list=args.text_string_list,
         height=args.height,
         step=args.step,
         padding=args.padding,
